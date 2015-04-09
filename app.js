e85b5ecd509e2e597bbc3ed467ae1b0ba549c36c
Ayush <ayush29f@uw.edu>
2015-04-08 19:22:05 -0700

    Twilio installed

--- app.js
+++ app.js
@@ -29,7 +29,16 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+
+var router = app.Router();
+// router.route('/sms')
+// 	.post(function(req, res) {
+		
+// 	})
+
+
+app.use('/twilio', router);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
