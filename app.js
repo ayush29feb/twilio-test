9d3284c9c656a836069985ccb4d1e63f70e4e470
Ayush <ayush29f@uw.edu>
2015-04-11 22:39:16 -0700

    trying

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/twilio', function(req, res){
 
 app.post('/twilio/reply', function(req, res, next){
 	var resp = new twilio.TwimlResponse();
-	resp.sms('ahoy hoy! Testing Twilio and node.js');
+	resp.sms(request.body.from + ' Hey');
 	res.writeHead(200, {
 		'Content-Type':'text/xml'
 	});
